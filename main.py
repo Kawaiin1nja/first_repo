1325ad5fb3ae5f98b6a3b00825f4b1e781604360
Roman Shuhol <romanshuhol@gmail.com>
2023-09-07 23:20:17 +0300

    add Multi-Line Comment

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print("Wake up, Neo...")
+'''In The Matrix, a 1999 film by the Wachowski brothers, a black-clad
+computer hacker known as Neo falls asleep in front of his computer. A mysterious
+message appears on the screen: "Wake up, Neo."'''
